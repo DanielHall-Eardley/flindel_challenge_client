afe1537742bd71963c11b70163eb06b0d15ee483
DanielHall-Eardley <350chevy8@gmail.com>
2020-08-17 20:51:38 -0400

    initial

---
+++ build/precache-manifest.65f74bda4abae752bf2389db86c04b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bec9436543af7f983be713837358b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8462dd5f67a630d84aa",
+    "url": "/static/css/main.971923ad.chunk.css"
+  },
+  {
+    "revision": "bd67c0272fd23836225a",
+    "url": "/static/js/2.0c3901df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c3901df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8462dd5f67a630d84aa",
+    "url": "/static/js/main.3fc7da30.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
